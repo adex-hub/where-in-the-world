2695defca3dabefc7f79128188835516ff65fb50
adex-hub <adeolaweb@gmail.com>
2025-09-27 21:01:37 +0100

    fix: update index variable name for clarity in CountryList component

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -6,8 +6,8 @@ function CountryList() {
 
   return (
     <div className="mt-10 w-full flex flex-col sm:gap-6 md:grid md:gap-6 md:grid-cols-2 lg:grid-cols-3 md:place-items-center xl:grid-cols-4 justify-center items-center">
-      {Array.from({ length: countryData.length }, (_, index) => (
-        <CountryCard key={index} index={index} />
+      {Array.from({ length: countryData.length }, (_, country) => (
+        <CountryCard key={country} index={country} />
       ))}
     </div>
   );
